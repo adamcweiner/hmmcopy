0c548f462493ea04471f0f04334a8d2e9b36e4f0
Adam Weiner <adamcweiner@gmail.com>
2020-07-10 17:22:45 -0400

    seems to be working?

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,5 +1,6 @@
 import subprocess
-import sys
+import os
+import multiprocessing
 import pandas as pd
 from argparse import ArgumentParser
 
@@ -16,20 +17,43 @@ def get_args():
 	return p.parse_args()
 
 
+def run(chunk):
+	argv = get_args()
+
+	for i, row in chunk.iterrows():
+		file = "{input_dir}/{library_id}_inputs.yaml".format(library_id=row['library_id'], input_dir=argv.input_dir)
+
+		if os.path.exists(file):
+			subprocess.call('single_cell hmmcopy --input_yaml {input_dir}/{library_id}_inputs.yaml \
+				--library_id {library_id} --maxjobs 4 --nocleanup --sentinel_only  \
+				--submit lsf --loglevel DEBUG \
+				--config_file config.yaml \
+				--tmpdir {temp_dir}/{library_id} \
+				--pipelinedir {pipeline_dir}/{library_id} --submit lsf --out_dir {output_dir}/{library_id}'.format(
+					library_id=row['library_id'], input_dir=argv.input_dir, output_dir=argv.output_dir,
+					temp_dir=argv.temp_dir, pipeline_dir=argv.pipeline_dir),
+				shell=True)
+
 
 def main():
 	argv = get_args()
 	df = pd.read_csv(argv.samples, sep='\t', index_col=False, dtype=str)
-	print(sys.version)
-	subprocess.call('sh final_run.sh', shell=True)
 
-	# for i, row in df.iterrows():
-	# 	subprocess.check_call('single_cell hmmcopy --input_yaml {input_dir}/{library_id}_inputs.yaml \
-	# 		--library_id {library_id} --maxjobs 4 --nocleanup --sentinel_only  \
-	# 		--submit lsf --loglevel DEBUG \
-	# 		--config_file config.yaml \
-	# 		--tmpdir {temp_dir}/{library_id} \
-	# 		--pipelinedir {pipeline_dir}/{library_id} --submit lsf --out_dir {output_dir}/{library_id}'.format(
-	# 			library_id=row['library_id'], input_dir=argv.input_dir, output_dir=argv.output_dir,
-	# 			temp_dir=argv.temp_dir, pipeline_dir=argv.pipeline_dir),
-	# 		shell=True)
+	# create as many processes as there are CPUs on your machine
+	num_processes = multiprocessing.cpu_count()
+
+	# calculate the chunk size as an integer
+	chunk_size = int(df.shape[0]/num_processes)
+
+	# separate df into chunks
+	chunks = [df.iloc[df.index[i:i + chunk_size]] for i in range(0, df.shape[0], chunk_size)]
+
+	# create our pool with `num_processes` processes
+	pool = multiprocessing.Pool(processes=num_processes)
+
+	# apply our function to each chunk in the list
+	result = pool.map(run, chunks)
+
+
+if __name__ == '__main__':
+	main()
